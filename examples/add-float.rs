f4dd00b3be3b7a2430b5d63c3da259f9ccb4dea2
Eric Holk <eholk@cs.indiana.edu>
2012-12-13 12:38:34 -0500

    Executing the add-float example kernel.

--- examples/add-float.rs
+++ examples/add-float.rs
@@ -1,6 +1,50 @@
 extern mod OpenCL;
 use OpenCL::hl::*;
+use OpenCL::CL::*;
 
 fn main() {
+    let kernel_name = "_ZN9add_float17_5ac86b11d5cd66c13_00E";
+
+    let platforms = get_platforms();
+    let devices = get_devices(platforms[0]);
+    let context = create_context(devices[0]);
+    let q = create_commandqueue(&context, devices[0]);
+
+    let A = ~[1f];
+    let B = ~[2f];
+    let C = ~[mut 0f];
+
+    let Ab = create_buffer(&context,
+                           sys::size_of::<float>() as int,
+                           CL_MEM_READ_ONLY);
+    let Bb = create_buffer(&context,
+                           sys::size_of::<float>() as int,
+                           CL_MEM_READ_ONLY);
+    let Cb = create_buffer(&context,
+                           sys::size_of::<float>() as int,
+                           CL_MEM_READ_ONLY);
     
+    enqueue_write_buffer(&q, &Ab, &A);
+    enqueue_write_buffer(&q, &Bb, &B);
+
+    let program = create_program_with_binary(
+        &context,
+        devices[0],
+        &path::Path("add-float-kernel.ptx"));
+
+    build_program(&program, devices[0]);
+
+    let kernel = create_kernel(&program, kernel_name);
+
+    set_kernel_arg(&kernel, 0, &ptr::null::<libc::c_void>());
+    set_kernel_arg(&kernel, 1, &ptr::null::<libc::c_void>());
+    set_kernel_arg(&kernel, 2, &Ab);
+    set_kernel_arg(&kernel, 3, &Bb);
+    set_kernel_arg(&kernel, 4, &Cb);    
+
+    enqueue_nd_range_kernel(&q, &kernel, 1, 0, 1 as int, 1);
+
+    enqueue_read_buffer(&q, &Cb, &C);
+
+    io::println(fmt!("%? + %? = %?", A, B, C));
 }
