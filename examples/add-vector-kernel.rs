724875134307a085ef1adfbd4b21d06d7bf674ad
Eric Holk <eric.holk@gmail.com>
2013-01-12 19:28:43 -0500

    Make add-vector-kernel cleaner.

--- examples/add-vector-kernel.rs
+++ examples/add-vector-kernel.rs
@@ -9,34 +9,19 @@ extern mod rusti {
     fn reinterpret_cast<T, U>(&&e: T) -> U;
 }
 
-//#[device]
-//fn offset(x: &float, i: uint) -> &float unsafe {
-//    let x: uint = rusti::reinterpret_cast(x);
-//    rusti::reinterpret_cast(&(x + i * 8))
-//}
+#[device]
+fn offset(x: &float, i: uint) -> &float unsafe {
+    let x: uint = rusti::reinterpret_cast(x);
+    rusti::reinterpret_cast((x + i * 8))
+}
 
 #[kernel]
 fn add_vector(x: &float, y: &float, z: &float) unsafe {
     let id = gpu::ptx_tid_x() as uint;
 
-    //let x = offset(x, id);
-    //let y = offset(y, id);
-    //let z: &mut float = rusti::reinterpret_cast(&offset(z, id));
-    
-    let x: &float = {
-        let x: uint = rusti::reinterpret_cast(x);
-        rusti::reinterpret_cast((x + id * 8))
-    };
-    
-    let y: &float = {
-        let x: uint = rusti::reinterpret_cast(y);
-        rusti::reinterpret_cast((x + id * 8))
-    };
-    
-    let z: &mut float = {
-        let x: uint = rusti::reinterpret_cast(z);
-        rusti::reinterpret_cast(x + id * 8)
-    };
+    let x = offset(x, id);
+    let y = offset(y, id);
+    let z: &mut float = rusti::reinterpret_cast(offset(z, id));
 
     *z = *x + *y
 }
