92928fd9aea626ffc2cf057451236c62174b2093
Milinda Pathirage <mpathira@indiana.edu>
2013-01-17 13:29:44 -0500

    improvements to benchmark code.

--- benchmarks/add-vector.rs
+++ benchmarks/add-vector.rs
@@ -13,10 +13,9 @@ fn main() {
     let kernel_name = "_ZN10add_vector17_b9e834807ab854383_00E";
 
     //let start = get_time();
+    let size: int = option::unwrap(int::from_str(os::args()[1]));
 
     let ctx = create_compute_context_types([GPU]);
-    let context = &ctx.ctx;
-    let q = &ctx.q;
     
     let A = DVec();
     let B = DVec();
@@ -24,7 +23,7 @@ fn main() {
     
     let mut i = 0;
     let r = rand::Rng();
-    while i < 4194304 {
+    while i < size {
     	  A.push(r.gen_float());
 	  B.push(r.gen_float());
 	  C.push(0f);	
@@ -32,18 +31,17 @@ fn main() {
     }
 
     let mut Ah = dvec::unwrap(move A);
+    let mut Bh = dvec::unwrap(move B);
     let Ab = Vector::from_vec(ctx, Ah);
-    let Bb = Vector::from_vec(ctx, dvec::unwrap(move B));
+    let Bb = Vector::from_vec(ctx, Bh);
     let Cb = Vector::from_vec(ctx, dvec::unwrap(move C));
     
-    let program = create_program_with_binary(
-        context,
-        ctx.device,
-        &path::Path("add-vector-kernel.ptx"));
+    let program = ctx.create_program_from_binary(
+        include_str!("add-vector-kernel.ptx"));
 
-    build_program(&program, ctx.device);
+    program.build(ctx.device);
 
-    let kernel = create_kernel(&program, kernel_name);
+    let kernel = program.create_kernel(kernel_name);
 
     kernel.set_arg(0, &0);
     kernel.set_arg(1, &0);
@@ -51,15 +49,21 @@ fn main() {
     kernel.set_arg(3, &Bb);
     kernel.set_arg(4, &Cb);    
 
-    let start = get_time();
-    enqueue_nd_range_kernel(q, &kernel, 1, 0, 4194304 * 8, 32);
+    info!("Starting vector add kernel");
 
-    let C = Cb.to_vec();
+    let start = get_time();
+    let local_size: int = 64;
+    let global_size: int = ((size + local_size - 1) / local_size) * local_size;
+    kernel.execute(global_size, local_size);
     
     let end = get_time();
 
+    info!("Vector add kernel complete");
+
+    let C = Cb.to_vec();
+    
     let elapsed = end.sec * 1000000000 + end.nsec as i64 - (start.sec * 1000000000 + start.nsec as i64);
 
-    io::println(fmt!("lapsed time: %?", elapsed));
-    io::println(fmt!("%? - %?", Ah[3194303],  C[3194303]));
+    io::println(fmt!("Size: %? Elapsed time: %? nanoseconds", size, elapsed));
+    io::println(fmt!("%? + %? = %?", Ah[size -1], Bh[size - 1],  C[size - 1]));
 }
--- benchmarks/mat-mult.rs
+++ benchmarks/mat-mult.rs
@@ -13,6 +13,7 @@ fn main() {
     let kernel_name = "_ZN8mat_mult16_bea2bb16c38860a3_00E";
 
     //let start = get_time();
+    let size: int = option::unwrap(int::from_str(os::args()[1]));
 
     let ctx = create_compute_context_types([GPU]);
     
@@ -22,7 +23,7 @@ fn main() {
     
     let mut i = 0;
     let r = rand::Rng();
-    while i < 16 {
+    while i < size * size {
     	A.push(r.gen_float());
 	    B.push(r.gen_float());
 	    C.push(0f);	
@@ -48,16 +49,20 @@ fn main() {
     kernel.set_arg(5, &4u);
 
     info!("Starting kernel.");
+
     let start = get_time();
-    kernel.execute((4, 4), (1, 1));
+
+    kernel.execute((size, size), (8, 8));
+
+    let end = get_time();
 
     let C = Cb.to_vec();
     
-    let end = get_time();
+
     info!("Kernel complete.");
 
     let elapsed = end.sec * 1000000000 + end.nsec as i64 - (start.sec * 1000000000 + start.nsec as i64);
 
-    io::println(fmt!("lapsed time: %?", elapsed));
-    io::println(fmt!("%? - %?", Ah[3194303],  C[3194303]));
+    io::println(fmt!("Mat mult kernel: %? Elapsed time: %?", size, elapsed));
+    io::println(fmt!("%? - %?", Ah[(size * size) -1],  C[(size * size) -1]));
 }
