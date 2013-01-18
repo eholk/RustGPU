7eaa94c3fb2c86effeab113f8c6adca245a96743
Eric Holk <eric.holk@gmail.com>
2013-01-18 11:58:00 -0500

    Run the benchmark twice in a row to see if the timings are consistent. They are.

--- benchmarks/rust/add-vector.rs
+++ benchmarks/rust/add-vector.rs
@@ -32,24 +32,32 @@ fn main() {
 
     n.push(size as uint);
 
-    let start = get_time();
+    let program = ctx.create_program_from_binary(
+        include_str!("add-vector-kernel.ptx"));
+    
+    program.build(ctx.device);
 
     let mut Ah = dvec::unwrap(move A);
     let mut Bh = dvec::unwrap(move B);
     let mut Ch = dvec::unwrap(move C);
     let mut Nh = dvec::unwrap(move n);
 
+    let kernel = program.create_kernel("add_vector");
+
+    benchmark(&kernel, ctx, size, Ah, Bh, Ch, Nh);
+    benchmark(&kernel, ctx, size, Ah, Bh, Ch, Nh);
+}
+
+fn benchmark(kernel: &Kernel, ctx: @ComputeContext,
+             size: int,
+             Ah: &[float], Bh: &[float], Ch: &[float], Nh: &[uint]) {
+
+    let start = get_time();
+
     let Ab = Vector::from_vec(ctx, Ah);
     let Bb = Vector::from_vec(ctx, Bh);
     let Cb = Vector::from_vec(ctx, Ch);
     let Nb = Vector::from_vec(ctx, Nh);
-
-    let program = ctx.create_program_from_binary(
-        include_str!("add-vector-kernel.ptx"));
-    
-    program.build(ctx.device);
-
-    let kernel = program.create_kernel("add_vector");
 
     let exec_start  = get_time();
 
